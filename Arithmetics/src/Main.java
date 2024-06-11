8fb8dff1af06204a45a927a2524d6247cc80d906
MADMAN-Modding <mad@kmdtech.net>
2024-06-11 15:40:01 -0400

    Arithmetics

---
+++ Arithmetics/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        int x = 34;
+
+        // Addition
+        int y = 1 + x;
+
+        System.out.println(y); // What's the output?
+
+        // Division
+        double z = x % y;
+
+        System.out.println(z);
+
+        int multiplication = x * y;
+
+        System.out.println(multiplication);
+
+        // Prints PI
+        System.out.println(Math.PI);
+
+        // Gives the value of x (34) to the power of 2
+        System.out.println(x^2);
+    }
+}
