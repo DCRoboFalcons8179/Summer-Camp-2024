70fc1259f5f896df1063f6769915ad0cfe98eff4
MADMAN-Modding <mad@kmdtech.net>
2024-06-11 16:14:59 -0400

    Data types

---
+++ Data Types/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        // A float stores fractional numbers with 6-7 decimals
+        float num0 = 123.12334f;
+
+        // A double store a number with decimals with 15 decimals (preferred)
+        double num1 = 123.125523;
+
+        // A short stores numbers from -32,768 to 32,767
+        short num2 = 123;
+        
+        // An int stores numbers from -2,147,483,648 to 2,147,483,647
+        int num3 = 1234;
+
+        // A long stores numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        long num4 = 2123211412;
+
+        // A boolean stores a value of true or false
+        boolean bool = true;
+    }
+}
